08d49c27f6f92afbe2ac451ab36fa53af49b1b0c
CivilBot <civilbots@gmail.com>
2018-07-06 10:00:32 +0530

    Refactored ITEM to Item

--- type-annotations/Exercise1.elm
+++ type-annotations/Exercise1.elm
@@ -1,28 +1,28 @@
 module Exercise1 exposing (..)
 import Html exposing (text)
 
-type alias ITEM =
+type alias Item =
     {
         name : String
     ,   qty : Int
     ,   freeQty : Int
     }
 
-cart : List ITEM
+cart : List Item
 cart =
     [ { name = "Lemon", qty = 1, freeQty = 0}
     , { name = "Apple", qty = 5, freeQty = 0}
     , { name = "Pear", qty = 10, freeQty = 0}
     ]
 
-getDiscount : Int -> Int -> ITEM -> ITEM
+getDiscount : Int -> Int -> Item -> Item
 getDiscount minQty freeQty item =
     if item.freeQty ==0 && item.qty >= minQty then
         {item | freeQty = freeQty}
     else
         item
 
-newCart : List ITEM
+newCart : List Item
 newCart =
     List.map ((getDiscount 10 3) >> (getDiscount 5 1)) cart
 
